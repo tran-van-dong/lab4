4a63efbb01309a2d49ad82e918dbdb67380505b8
tran-van-dong <146536282+tran-van-dong@users.noreply.github.com>
2023-10-13 14:56:10 +0700

    Add files via upload

---
+++ OrderItem.java
@@ -0,0 +1,12 @@
+package lab3;
+
+public class OrderItem {
+		private Product p;
+		private int quality;
+		public OrderItem(Product p, int quality) {
+			super();
+			this.p = p;
+			this.quality = quality;
+		}
+
+}
